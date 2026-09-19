d656bfbdf6d9b7e437c9400300ae1da748537ede
Youssef Alaa <ya3851315@gmail.com>
2026-09-19 20:12:11 +0300

    Add Nutrition page with dynamic meal plans and update Navbar drawer and supplements shop

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,9 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar';
 import { Home } from './components/home/home';
 import { AboutAs } from './components/about-as/about-as';
+import { ContactUs } from './components/contact-us/contact-us';
+import { Nutrition } from './components/nutrition/nutrition';
+import { SupplementStore } from './components/supplement-store/supplement-store';
 
 @Component({
-imports: [RouterOutlet, Home, NavbarComponent, AboutAs],
+imports: [RouterOutlet, Home, NavbarComponent, AboutAs, ContactUs, Nutrition, SupplementStore],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
